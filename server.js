fd229a14d7543b8b88cfe1b283f46bb22d526391
Voroshilov Daniil <sssk1led@gmail.com>
2023-07-12 22:13:06 +0300

    аыв

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const express = require('express')
 const server = express()
 const fs = require('fs')
 const path = require('path')
-const PORT = 8888 //введи цифры и изменишь порт)
+const PORT = 8888 //ПОРТ
 
-const urlPars = express.urlencoded({extended: false}) //парс структуры
-const createPath = (file) => path.resolve(__dirname, 'views', file)
-
+const urlPars = express.urlencoded({extended: false}) //ПАРС ОТПРАВЛЯЕМЫХ ДАННЫХ REQUIRE
+const createPath = (file) => path.resolve(__dirname, 'views', file) //ОТНОСИТЕЛЬНЫЙ ПУТЬ К ФАЙЛ
 
 server.use(express.static('views'))
 
